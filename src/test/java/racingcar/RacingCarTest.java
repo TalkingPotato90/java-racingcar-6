8c66bdc53470f1c27e6e6ffe4b1b3acf908ec985
TalkingPotato90 <star901210@hanmail.net>
2024-10-04 16:17:52 +0900

    Test: 무작위로 생성한 숫자 개수가 자동차 개수와 일치하는지 확인하는 테스트 구현

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("레이싱카 테스트")
@@ -28,5 +29,10 @@ class RacingCarTest {
     @Test
     void 횟수가_10을_넘으면_예외(){
         assertThatThrownBy(()->racingCar.validateTryCounts("11")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 지정된_개수만큼_무작위_숫자_생성(){
+        assertThat(racingCar.createRandomNumber()).hasSize();
     }
 }
