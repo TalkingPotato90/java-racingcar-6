c75fe6f48360f39039df6ed1b08d4504238dea80
TalkingPotato90 <star901210@hanmail.net>
2024-10-04 16:46:40 +0900

    Feat: 사용자 입력 쉼표 기준 분할

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -3,6 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -14,21 +15,26 @@ public class RacingCar {
     }
 
     public void validateTryCounts(String inputCounts) {
-        if(!inputCounts.matches("^[0-9]*$")){
+        if (!inputCounts.matches("^[0-9]*$")) {
             throw new IllegalArgumentException("[ERROR] 시도할 횟수는 숫자만 입력가능합니다.");
         }
 
-        if(Integer.parseInt(inputCounts) > 10){
+        if (Integer.parseInt(inputCounts) > 10) {
             throw new IllegalArgumentException("[ERROR] 시도할 횟수는 10회 이하로만 가능합니다.");
         }
     }
 
-    public List<Integer> createRandomNumber(int player){
+    public List<Integer> createRandomNumber(int player) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < player; i++) {
-            numbers.add(Randoms.pickNumberInRange(0,9));
+            numbers.add(Randoms.pickNumberInRange(0, 9));
         }
 
         return numbers;
+    }
+
+    public List<String> createPlayersCar(String inputNames) {
+        List<String> players = new ArrayList<>(Arrays.asList(inputNames.split(",")));
+        return players;
     }
 }
