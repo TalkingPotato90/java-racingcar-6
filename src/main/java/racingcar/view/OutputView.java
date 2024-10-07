22d0211d1bf87a721ac38a537ff0e312240cda48
TalkingPotato90 <star901210@hanmail.net>
2024-10-07 14:43:42 +0900

    Feat: 게임 시작 문구 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,18 @@
 package racingcar.view;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class OutputView {
+    public void print() {
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            br.write("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            br.flush();
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
